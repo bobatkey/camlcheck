3bda39ded1fc9c00847961760e44bd4f70fc8a25
Robert Atkey <bob.atkey@gmail.com>
2013-04-14 23:31:38 +0100

    Shrinking of counterexamples, API redesign
    - Flesh out the Generators
    
    - Rename Arbitrary to Domain, and add shrinking of counterexamples
    
    - Move the implementations for the primitive type random generators to a
      PrimitiveDomains module
    
    - Redo the API, and include a bridge to OUnit

--- lib/CamlCheck.ml
+++ lib/CamlCheck.ml
@@ -1,51 +1,83 @@
-type 'a generator =
-    Random.State.t -> 'a
+module Generator = struct
+  type 'a t = Random.State.t -> 'a
 
-type 'a arbitrary =
-    { generate    : 'a generator
-    ; to_string   : ('a -> string)
-    ; description : string
-(*    ; shrink    : ('a -> 'a list)*)
-    }
+  let return a random_state = a
+  let bind c f random_state =
+    f (c random_state) random_state
 
-type property = Random.State.t -> unit
+  let lift2 f g1 g2 =
+    bind g1 begin fun x1 ->
+      bind g2 begin fun x2 ->
+        return (f x1 x2)
+      end
+    end
 
-exception Counter_example of (string * string) list
+  let lift f g =
+    bind g begin fun x -> return (f x) end
 
-let forall domain predicate random_state =
-  let value = domain.generate random_state in
-  try predicate value random_state
-  with Counter_example l ->
-    let str  = domain.to_string value in
-    let desc = domain.description in
-    raise (Counter_example ((str,desc)::l))
+  let bits random_state =
+    Random.State.bits random_state
 
-let check_property property =
-  let random_state = Random.State.make_self_init () in
-  try
-    for i = 1 to 1000 do property random_state done;
-    `OkAsFarAsIKnow
-  with Counter_example l ->
-    `CounterExample l
+  let int bound random_state =
+    Random.State.int random_state bound
 
-let check b _ =
-  if not b then raise (Counter_example [])
+  let int32 bound random_state =
+    Random.State.int32 random_state bound
 
-let ( ^$ ) f x = f x
+  let nativeint bound random_state =
+    Random.State.nativeint random_state bound
 
-module Arbitrary = struct
+  let int64 bound random_state =
+    Random.State.int64 random_state bound
+
+  let float bound random_state =
+    Random.State.float random_state bound
+
+  let bool random_state =
+    Random.State.bool random_state
+end
+
+module Domain = struct
+  type 'a t =
+      { generate    : 'a Generator.t
+      ; to_string   : ('a -> string)
+      ; description : string
+      ; shrink    : ('a -> 'a list)
+      }
+
+  let make ~generate ?shrink ~to_string ~description () =
+    match shrink with
+      | None ->
+        { generate; to_string; description; shrink = (fun _ -> []) }
+      | Some shrink ->
+        { generate; to_string; description; shrink }
+
+  let generator a = a.generate
+
+  let shrinker a = a.shrink
+
+  let description a = a.description
+
+  let to_string a = a.to_string
+end
+
+module PrimitiveDomains = struct
+  open Domain
+
   let unit =
     let generate random_state = ()
     and to_string () = "()"
     and description = "unit"
-    in {generate; to_string; description}
+    and shrink () = []
+    in {generate; to_string; description; shrink}
 
   let bool =
     let generate random_state = Random.State.bool random_state
     and to_string = function true -> "true" | false -> "false"
     and description = "bool"
+    and shrink b = []
     in
-    {generate; to_string; description}
+    {generate; to_string; description; shrink}
 
   let list a =
     let generate random_state =
@@ -58,22 +90,35 @@ module Arbitrary = struct
     and to_string l =
       "[" ^ String.concat "," (List.map a.to_string l) ^ "]"
     and description = a.description ^ " list"
+    and shrink l =
+      (* Return all the lists obtained by removing a single element
+         from [l] *)
+      let rec gather lists before after = match after with
+        | [] -> lists
+        | x::xs ->
+          gather ((List.rev before @ xs) :: lists) (x::before) xs
+      in
+      gather [] [] l
     in
-    {generate; to_string; description}
+    {generate; to_string; description;shrink}
 
   let char =
     let generate random_state =
       Char.chr (Random.State.int random_state 256)
     and to_string c = "\"" ^ Char.escaped c ^ "\""
-    and description = "char" in
-    {generate; to_string; description}
+    and description = "char"
+    and shrink c = []
+    in
+    {generate; to_string; description; shrink}
 
   let printable_ascii_char =
     let generate random_state =
       Char.chr (Random.State.int random_state (128 - 32) + 32)
     and to_string c = "\"" ^ Char.escaped c ^ "\""
-    and description = "printable_ascii_char" in
-    {generate; to_string; description}
+    and description = "printable_ascii_char"
+    and shrink c = []
+    in
+    {generate; to_string; description; shrink}
 
   let string =
     let generate random_state =
@@ -84,8 +129,19 @@ module Arbitrary = struct
       done;
       str
     and to_string s = "\"" ^ String.escaped s ^ "\""
-    and description = "string" in
-    {generate; to_string; description}
+    and description = "string"
+    and shrink s =
+      let l = String.length s in
+      let ss = ref [] in
+      for i = 0 to l - 1 do
+        let s' = String.create (l - 1) in
+        String.blit s 0 s' 0 i;
+        String.blit s (i+1) s' i (l-i-1);
+        ss := s' :: !ss
+      done;
+      !ss
+    in
+    {generate; to_string; description; shrink}
 
   let printable_ascii_string =
     let generate random_state =
@@ -96,8 +152,19 @@ module Arbitrary = struct
       done;
       str
     and to_string s = String.escaped s
-    and description = "printable_ascii_string" in
-    {generate; to_string; description}
+    and description = "printable_ascii_string"
+    and shrink s =
+      let l = String.length s in
+      let ss = ref [] in
+      for i = 0 to l - 1 do
+        let s' = String.create (l - 1) in
+        String.blit s 0 s' 0 i;
+        String.blit s (i+1) s' i (l-i-1);
+        ss := s' :: !ss
+      done;
+      !ss
+    in
+    {generate; to_string; description; shrink}
 
   let array a =
     let generate random_state =
@@ -114,8 +181,18 @@ module Arbitrary = struct
       Buffer.add_string b "|]";
       Buffer.contents b
     and description = a.description ^ " array"
+    and shrink a =
+      let l = Array.length a in
+      let arrays = ref [] in
+      for i = 0 to l - 1 do
+        let a' =
+          Array.init (l-1) (fun j -> if j < i then a.(j) else a.(j+1))
+        in
+        arrays := a' :: !arrays
+      done;
+      !arrays
     in
-    {generate; to_string; description}
+    {generate; to_string; description; shrink}
 
   let option a =
     let generate random_state =
@@ -127,8 +204,12 @@ module Arbitrary = struct
       | None -> "None"
       | Some v -> "Some (" ^ a.to_string v ^ ")"
     and description = a.description ^ " option"
+    and shrink = function
+      | None -> []
+      | Some v ->
+        None :: List.map (fun x -> Some x) (a.shrink v)
     in
-    {generate;to_string;description}
+    {generate; to_string; description; shrink}
 
   let int_range minimum maximum =
     let generate random_state =
@@ -137,6 +218,87 @@ module Arbitrary = struct
     and to_string i = string_of_int i
     and description =
       Printf.sprintf "int_range(%d,%d)" minimum maximum
+    and shrink i =
+      if i > minimum then [i-1] else []
     in
-    {generate; to_string; description}
+    {generate; to_string; description; shrink}
+
+  let float_range minimum maximum =
+  (* FIXME: this is wrong... *)
+    Domain.make
+      ~generate:(Generator.bind (Generator.float (maximum -. minimum)) (fun f -> Generator.return (f +. minimum)))
+      ~to_string:string_of_float
+      ~description:(Printf.sprintf "float(%f,%f)" minimum maximum)
+      ()
+end
+
+module Property = struct
+  open Domain
+
+  type t =
+      Random.State.t ->
+      [ `Ok | `CounterExample of ((string * string) list * string) ]
+
+  let forall domain predicate random_state =
+    let initial_value = domain.generate random_state in
+    let saved_random_state = Random.State.copy random_state in
+    let make_report value =
+      (domain.to_string value,
+       domain.description)
+    in
+    let rec try_shrinkings = function
+      | [] -> None
+      | value::values ->
+        let random_state = Random.State.copy saved_random_state in
+        match predicate value random_state with
+          | `Ok               -> try_shrinkings values
+          | `CounterExample l -> Some (try_to_shrink l value)
+    and try_to_shrink (l,msg) value =
+      match try_shrinkings (domain.shrink value) with
+        | None        -> (make_report value::l, msg)
+        | Some report -> report
+    in
+    match predicate initial_value random_state with
+      | `Ok ->
+        `Ok
+      | `CounterExample report ->
+        `CounterExample (try_to_shrink report initial_value)
+
+  let test_property property =
+    let random_state = Random.State.make_self_init () in
+    let rec run i =
+      if i = 0 then `OkAsFarAsIKnow
+      else match property random_state with
+        | `Ok ->
+          run (i-1)
+        | `CounterExample report ->
+          `CounterExample report
+    in
+    run 10000
+
+  let check b _ =
+    if b then `Ok else `CounterExample ([], "")
+
+  let check_equal ~to_string x1 x2 _ =
+    if x1 = x2 then
+      `Ok
+    else
+      let message = "Not equal: " ^ to_string x1 ^ " and " ^ to_string x2 in
+      `CounterExample ([], message)
+
+  let check_raises exn f =
+    check (try f (); false with exn' when exn = exn' -> true | _ -> false)
+
+  let test_of_property property () =
+    match test_property property with
+      | `OkAsFarAsIKnow -> ()
+      | `CounterExample (l, msg) ->
+        let message =
+          (* FIXME: better rendering of the result *)
+          "Counterexample: " ^ msg ^ "\n"
+          ^ String.concat "\n" (List.map (fun (x,y) -> "(" ^ x ^ "," ^ y ^ ")") l)
+        in
+        OUnit.assert_failure message
+
+  let ( ^$ ) f x = f x
 end
--- lib/CamlCheck.mli
+++ lib/CamlCheck.mli
@@ -1,33 +1,100 @@
-type 'a arbitrary
+(** Randomised Property Checking *)
 
-type property
+(**{1 Randomised Property Checking}
 
-val forall : 'a arbitrary -> ('a -> property) -> property
+   FIXME: intro
 
-val check_property : property -> [ `OkAsFarAsIKnow | `CounterExample of (string * string) list ]
+   FIXME: explain relationship between Generators and Domains.
+*)
 
-val check : bool -> property
+(** Generators for random test data. *)
+module Generator : sig
+  type 'a t
 
-val ( ^$ ) : ('a -> 'b) -> 'a -> 'b
+  val return : 'a -> 'a t
+  val bind   : 'a t -> ('a -> 'b t) -> 'b t
 
-module Arbitrary : sig
-  val unit : unit arbitrary
+  val lift : ('a -> 'b) -> 'a t -> 'b t
+  val lift2 : ('a -> 'b -> 'c) -> 'a t -> 'b t -> 'c t
 
-  val bool : bool arbitrary
+  val bits : int t
+  val int : int -> int t
+  val int32 : int32 -> int32 t
+  val nativeint : nativeint -> nativeint t
+  val int64 : int64 -> int64 t
+  val float : float -> float t
+  val bool : bool t
+end
 
-  val int_range : int -> int -> int arbitrary
+(** Domains of randomly generated test data. *)
+module Domain : sig
+  type 'a t
 
-  val list : 'a arbitrary -> 'a list arbitrary
+  val make :
+    generate:('a Generator.t) ->
+    ?shrink:('a -> 'a list) ->
+    to_string:('a -> string) ->
+    description:string ->
+    unit ->
+    'a t
 
-  val char : char arbitrary
+  val generator   : 'a t -> 'a Generator.t
+  val shrinker    : 'a t -> 'a -> 'a list
+  val description : 'a t -> string
+  val to_string   : 'a t-> 'a -> string
+end
 
-  val printable_ascii_char : char arbitrary
+(** Universally quantified properties for randomised testing. *)
+module Property : sig
+  type t
 
-  val string : string arbitrary
+  val forall : 'a Domain.t -> ('a -> t) -> t
 
-  val printable_ascii_string : string arbitrary
+  val check : bool -> t
 
-  val array : 'a arbitrary -> 'a array arbitrary
+  val check_equal : to_string:('a -> string) -> 'a -> 'a -> t
 
-  val option : 'a arbitrary -> 'a option arbitrary
+  val check_raises : exn -> (unit -> 'a) -> t
+
+  (** Tests the given property 10000 times with random data for each
+      universally quantified variable. If a counterexample is
+      discovered, then an attempt to shrink it to produce a smaller
+      refutation of the property. If no counterexample is found, then
+      [`OkAsFarAsIKnow] is returned. *)
+  val test_property : t ->
+    [ `OkAsFarAsIKnow
+    | `CounterExample of ((string * string) list * string) ]
+
+  (** Turns a property into an OUnit test case. {!check_property} is
+      invoked on the property. If a counterexample is discovered then the
+      test fails. *)
+  val test_of_property : t -> unit -> unit
+
+  (** A useful combinator for writing properties. *)
+  val ( ^$ ) : ('a -> 'b) -> 'a -> 'b
+end
+
+(** Domains for some standard types. *)
+module PrimitiveDomains : sig
+  val unit : unit Domain.t
+
+  val bool : bool Domain.t
+
+  val int_range : int -> int -> int Domain.t
+
+  val float_range : float -> float -> float Domain.t
+
+  val list : 'a Domain.t -> 'a list Domain.t
+
+  val char : char Domain.t
+
+  val printable_ascii_char : char Domain.t
+
+  val string : string Domain.t
+
+  val printable_ascii_string : string Domain.t
+
+  val array : 'a Domain.t -> 'a array Domain.t
+
+  val option : 'a Domain.t -> 'a option Domain.t
 end
